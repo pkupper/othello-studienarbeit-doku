d57c7d0cb0a4a6208f805c14452c25c47b7bb4fa
tomaThomas <thomas.moeller@mailbox.org>
2020-04-21 23:49:28 +0200

    Heuristic results WIP

--- listings/alphabeta.py
+++ listings/alphabeta.py
@@ -1,20 +1,15 @@
-def alphabeta(state, depth, alpha, beta, heuristic):
+def alphabeta(state, alpha, beta, heuristic):
     if(state.game_over):
-        return get_winner(state)
-    if(depth <= 0):
-        return heuristic(state)
-
+        return utility(state, state.turn)
     if state.turn == WHITE:
         # maximizing
         utility = -math.inf
     else:
         # minimizing
-        utility = math.inf
-        
+        utility = math.inf  
     for move in get_possible_moves(state, state.turn):
-        tmp_state = make_move(copy.deepcopy(state), move[0], move[1])
-        tmp_utility = alphabeta(tmp_state, depth - 1, alpha, beta, heuristic)
-        
+        tmp_state = make_move(copy.deepcopy(state), move)
+        tmp_utility = alphabeta(tmp_state, alpha, beta, heuristic)
         if state.turn == WHITE:
             # maximizing
             utility = max(utility, tmp_utility)
